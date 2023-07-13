c4650938a1abd923e755c33205dc269dba46e2b2
1900018250 <1900018250@qq.com>
2023-07-13 17:28:04 +0800

    go

---
+++ main.js
@@ -0,0 +1,43 @@
+const { ethers} = require('ethers')
+const log4js = require('log4js');
+const ALCHEMY_MAINNET_WSSURL = 'ws://5.9.115.186:8546';
+const provider = new ethers.providers.WebSocketProvider(ALCHEMY_MAINNET_WSSURL);
+const provider1 = new ethers.providers.JsonRpcProvider('http://5.9.115.186:8545');
+
+
+log4js.configure({
+  appenders: {
+    hash: { type: 'file', filename: './logs/hash',     pattern: '-yyyy-MM-dd-hh-mm.log', // 每分钟创建一个新文件
+      alwaysIncludePattern: true,},
+    tx: { type: 'file', filename: './logs/tx',     pattern: '-yyyy-MM-dd-hh-mm.log', // 每分钟创建一个新文件
+      alwaysIncludePattern: true, },
+    console: { type: 'console' },
+  },
+  categories: {
+    hash: { appenders: ['hash', 'console'], level: 'debug' },
+    tx: { appenders: ['tx', 'console'], level: 'debug' },
+    default: { appenders: ['console'], level: 'debug' },
+  },
+});
+
+const hashLogger = log4js.getLogger('hash');
+const txLogger = log4js.getLogger('tx');
+
+
+const main = () => {
+  console.log("\n4. 监听pending交易，获取txHash，并输出交易详情。")
+  provider.on("pending", (txHash, ) => {
+    hashLogger.debug(txHash);
+    provider1.getTransaction(txHash).then((tx) => {
+      if (tx) {
+        // tx.timestamp
+        txLogger.debug(`hash: ${tx.hash} ==== ${tx.from}`)
+      }
+    }).catch(e => {console.log(e)})
+  });
+};
+
+
+
+main()
+
