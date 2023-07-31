318872d8656b02e1b0ba97c7bf6212f32319bcf4
1900018250 <1900018250@qq.com>
2023-07-31 14:39:36 +0800

    go

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as Rxjs from 'rxjs';
 import * as ops from 'rxjs/operators';
 import { hashLogger, txLogger } from './util/log.js';
 import usdtAbiJson from './usdtAbi.json' assert { type: 'json' };
+import _ from 'lodash'
 import {BehaviorSubject, of} from 'rxjs'
 import { isIncludedInAddressList } from './util/isIncludedInAddressList.js'
 // 5.9.115.186 127.0.0.1
@@ -116,7 +117,7 @@ const parseTxResData = (data, abi) => {
   } catch (e) {
     // 对错误进行更详细的处理，可能包括记录日志、发送警报等
     console.error(`Failed to parse and log transaction: ${e}`);
-    return false;
+    return null
   }
 }
 
@@ -141,28 +142,40 @@ const handleTxResParseData = (data, to) => {
   const abi = addressAbis
     .find(addressAbi => addressAbi.address.toLowerCase() === to.toLowerCase())?.abi ?? null
   if (!abi) return null
-  const parsedData = parseTxResData(data, abi)
-  return parsedData ? parsedData : null
+  return parseTxResData(data, abi)
 }
 
 const handleTxResByDataCondition$ = (parseData, condition) => {
-  const sighash = parseData.sighash
-  const conditionSighash = condition.sighash
-  if (!(sighash === conditionSighash)) {
+  // 获取解析后数据 和 条件的函数sighash标识
+  const sighash = _.get(parseData, 'sighash', null)
+  const conditionSighash = _.get(condition, 'sighash', null)
+  // 如果没有获取到函数标识
+  if (_.isNull(sighash) || _.isNull(conditionSighash)) {
     return Rxjs.EMPTY
   }
-  const inputs = parseData.functionFragment.inputs
-  const conditionInputs = condition.inputs
+  // 如果函数标识不一样
+  if (sighash !== conditionSighash) {
+    return Rxjs.EMPTY
+  }
+  // 解析后数据到inputs
+  const inputs =_.get(parseData, 'functionFragment.inputs', null)
+  // 条件里到inputs
+  const conditionInputs = _.get(condition, 'inputs', null)
+  // TODO 如果inputs为[] 或者 conditionInputs 为 []的情况
   for (let input of inputs) {
+    // 从解析后数据到input中获取当前要对比到变量名 和 变量类型
     const { name, type } = input
+    // 从条件里找到对应到变量要求
     const conditionArg = conditionInputs.find(conditionInput => conditionInput.name === name)?.value ?? null
-    if (!conditionArg) {
+    if (_.isNull(conditionArg)) { // 如果没有这个条件要求就直接跳过对比
       continue
     }
-    const parseDataArg = parseData.args[name] ?? null
-    if (!parseDataArg) {
+    // 取出解析里面到变量值
+    const parseDataArg = _.get(parseData, `args.${name}`, null)
+    if (_.isNull(parseDataArg)) { // 如果没有获取这个值 直接返回空
       return Rxjs.EMPTY
     }
+    // 进行对比
     if (!filterArgInType(parseDataArg, conditionArg, type)) {
       return Rxjs.EMPTY
     }
@@ -179,9 +192,11 @@ const handleTxResByDataCondition$ = (parseData, condition) => {
 const handleTxRes$ = (txRes, txFilter) => {
   const { hash ,type ,from, to, value, gasPrice, maxPriorityFeePerGas, maxFeePerGas, gasLimit, data, nonce } = txRes
   const { name: txFilterName, fromList: txFilterFromList, toList: txFilterToList, conditions} = txFilter
+  // 对比地址数据
   if (!handleTxResFilterByFromTo(from, to, txFilterFromList, txFilterToList)) {
     return Rxjs.EMPTY
   }
+  // 解析txRes.data 如果解析成功返回数据 失败返回null
   const parsedData= handleTxResParseData(data, to)
 
   return Rxjs.from(conditions).pipe(
@@ -197,9 +212,13 @@ const handleTxRes$ = (txRes, txFilter) => {
 //gasPrice:gasLimit:to:value:nonce:nonce:r:s:v:creates:chainId:wait:
 const processedTransaction$ = pendingTransaction$.pipe(
   // ops.tap(txHash => hashLogger.info(txHash)),
+  // 获取hash详情
   ops.mergeMap(txHash => httpProvider.getTransaction(txHash)),
-  ops.filter(txRes => txRes),
+  // 验证hash是否有数据
+  ops.filter(txRes => !!txRes),
+  // txRes => [{txFilter1, res}, {txFilter2, res}, ... ]
   ops.mergeMap(txRes => txFilters.map(txFilter => ({txRes, txFilter}))),
+  // 处理
   ops.mergeMap(({txRes, txFilter}) => handleTxRes$(txRes, txFilter)),
 )
 
@@ -208,7 +227,25 @@ processedTransaction$.subscribe({
   complete: () => console.log('完成')
 })
 
+// var users = [
+//   { 'user': 'barney',  'age': 36, 'active': true },
+//   { 'user': 'fred',    'age': 40, 'active': false },
+//   { 'user': 'pebbles', 'age': 1,  'active': true }
+// ];
+//
+// // _.find(users, function(o) { return o.age < 40; });
+// // => object for 'barney'
+//
+// // The `_.matches` iteratee shorthand.
+// console.log( _.find(users, { 'age': 2, 'active': true },));
+// => object for 'pebbles'
 
+
+// console.log( _.get({'a': 1}, 'b', null))
+// console.log(_.isNull(null))
+// console.log(_.isNull(false))
+// console.log(_.isNull(undefined))
+// console.log(_.isNull(NaN))
 // handleTxResByDataCondition$(
 //   parseTxResData(
 //     '0xa9059cbb0000000000000000000000006a747382ce8eb0255294e141d45eefd8b1748ed1000000000000000000000000000000000000000000000000000000b471513260',
